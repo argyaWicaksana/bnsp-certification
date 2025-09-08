1a5ba994047f5b8a2f16002d0f96734381653c8c
argyaWicaksana <argya7@gmail.com>
2025-09-08 15:17:58 +0700

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import SidebarLayout from '@/layouts/SidebarLayout'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <SidebarLayout>
+        <p>hello worsdasdld</p>
+      </SidebarLayout>
+    </>
+  )
+}
+
+export default App
